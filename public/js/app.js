55a5ff52aefa188292d04450642193876e4af01a
Erik Cunningham <djtrinary@gmail.com>
2015-09-24 00:26:05 -0600

    ewc: ok yeah this is fine. TODO: adjust to radians

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,43 @@
-
-(function() {
-  "use strict";
+(() => {
+  'use strict';
   var socket = io(),
-      svg = d3.select("body").append("svg"),
-      duration = 30000,
+      svg = d3.select('body').append('svg'),
+      duration = 60000,
       w = window.innerWidth,
       h = window.innerHeight,
-      variance = duration / 4;
+      variance = duration / 1000;
 
-  var pos = function(d, r) {
-    return [Math.sin(d * Math.PI / 180) * r,
-            Math.cos(d * Math.PI / 180) * r];
+  var pos = (d, r) => {
+    return [Math.cos(d) * r,
+            Math.sin(d) * r];
   };
 
   svg.attr({
     width: window.innerWidth,
     height: window.innerHeight
   });
-  var center = svg.append("g").attr("transform", "translate(" + [w/2,h/2] + ")");
+  var center = svg.append('g').attr('transform', 'translate(' + [w/2,h/2] + ')');
 
 
   var radius = (w - h > 0) ? (h / 2 - 50) : (w / 2 - 50);
-  var radScale = d3.scale.linear().domain([0,1]).range([25, radius]);
+  var radScale = d3.scale.linear().domain([0,60]).range([0, 2 * Math.PI]);
 
-  socket.on('tweet', function(tweet) {
-    var posted = new Date(tweet.postedTime);
-    var poster = { 
-      id: +(tweet.actor.id.split(':')[2]),
-      type: 'tweet'
-    };
-    var mentions = tweet.twitter_entities.user_mentions.map(function(m) {
-      return {
-        id: +m.id,
-        type: 'mention'
-      };
-    });
-    center.append("circle").classed("tweet", true)
+  socket.on('tweet', (tweet) => {
+    var posted = new Date(tweet.postedTime),
+        tweetPos = pos(posted % duration, radius),
+        poster = { 
+          id: +(tweet.actor.id.split(':')[2]),
+          type: 'tweet'
+        },
+        mentions = tweet.twitter_entities.user_mentions.map((m) => {
+          return {
+            id: +m.id,
+            type: 'mention'
+          };
+        });
+
+    console.log(posted % duration);
+    center.append('circle').classed('tweet', true)
       .attr({
         cx: 0,
         cy: 0,
@@ -44,28 +46,26 @@
       .transition()
         .duration(duration)
         .attr({
-          cx: pos((posted) % duration, radius)[0],
-          cy: pos((posted) % duration, radius)[1]
+          cx: tweetPos[0],
+          cy: tweetPos[1]
         })
         .remove();
-    mentions.forEach(function(m) {
-      var mod = (Math.random() - 0.5) * (duration / 4000);
-      mod = (mod > 0) ? mod + 10 : mod - 10;
-      console.log(mod);
-      center.append("path").classed("arc", true)
-        .attr({
-          d: "M0,0 T0,0"
-        })
+    mentions.forEach((m) => {
+      var mod = (Math.random() - 0.5) * variance,
+          mentionPos = pos((posted % duration) + (mod > 0) ? mod + 20 : mod - 10, radius);
+
+      center.append('path').classed('arc', true)
+        .attr('d', 'M0,0 T0,0' )
       .transition()
         .duration(duration)
         .attr({
-          d: "M" + pos((posted) % duration, radius)[0] + "," + 
-                   pos((posted) % duration, radius)[1] + " " + 
-             "T" + pos(((posted) % duration) + mod, radius)[0] + "," + 
-                   pos(((posted) % duration) + mod, radius)[1] + " "
+          d: 'M' + tweetPos[0] + ',' + 
+                   tweetPos[1] + ' ' + 
+             'T' + pos(((posted) % duration) + mod, radius)[0] + ',' + 
+                   pos(((posted) % duration) + mod, radius)[1] + ' '
         })
       .remove();
-      center.append("circle").classed("mention", true)
+      center.append('circle').classed('mention', true)
       .attr({
         cx: 0,
         cy: 0,
@@ -80,6 +80,4 @@
         .remove();
     });
   });
-
-  
 }());
